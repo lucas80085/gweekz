b8810ab04cb4b0669faf8656528747e27f01e4b8
lucas80085 <88892063+lucas80085@users.noreply.github.com>
2021-08-20 20:19:30 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.db.models.fields import SlugField
+from django.utils import translation
+
+
 
 
 class User(AbstractUser):
@@ -71,3 +75,67 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+
+#Noah
+
+class Game(models.Model):
+    different_games = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.different_games
+
+class Tag(models.Model):
+   caption = models.CharField(max_length=30) 
+
+   def __str__(self):
+       return self.caption
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(null = True,max_length=20000)
+    image = models.ImageField(null = True , blank = True)
+    price = models.FloatField()
+    games = models.ManyToManyField(Game)
+    tags = models.ManyToManyField(Tag)
+    slug = SlugField(unique= True)
+
+    def __str__(self):
+        return self.name
+    #Render Images mit einer Model Methode um entweder das Image zu rendern oder ein leeren string, damit kein Error Code entsteht 
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url 
+        except:
+            url = ""
+        return url
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null = True, blank = True, on_delete= models.CASCADE)
+    description = models.CharField(null = True,max_length=20000)
+    email = models.CharField(max_length=200)
+    image = models.ImageField(null = True , blank = True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null = True)
+
+    def __str__(self):
+     return self.user
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null =True, blank = True)
+    date_ordered = models.DateTimeField(auto_now_add = True)
+    complete = models.BooleanField(default = False)
+    transaction_id = models.CharField(max_length= 100, null = True)
+    
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete= models.SET_NULL, null = True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null= True)
+    quantity = models.IntegerField(default=0, null = True , blank = True)
+    date_added = models.DateTimeField(auto_now_add=True)
