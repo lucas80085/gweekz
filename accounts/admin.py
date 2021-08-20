173d4c0505eba60b2b948e9a676721ec11c5372c
lucas80085 <88892063+lucas80085@users.noreply.github.com>
2021-08-20 20:18:40 +0200

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Creator, Consumer#, Profile
+from .models import Creator, Consumer, Customer, Order, OrderItem, Product, Tag, Game
+
+from django.contrib import admin
 
 class ConsumerAdmin(admin.ModelAdmin):
     fields = ['first_name', 'last_name', 'username', 'email', 'password', 'avatar']
@@ -15,3 +17,14 @@ admin.site.register(Creator, CreatorAdmin)
 
 
 #admin.site.register(Profile)
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Tag)
+admin.site.register(Game)
